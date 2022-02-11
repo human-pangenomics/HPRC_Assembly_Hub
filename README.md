b9cd0b482b880ee138e560a8df1fff043413c95e
Julian Lucas <juklucas@ucsc.edu>
2022-02-11 11:48:05 -0800

    add instructions, fix MT issue, fix repeated imports

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # HPRC_Assembly_Hub
+
+## Prerequisites
+
+We are currently using an EC2 instance to serve the browser files:
+- ubuntu 18.04
+- t3.medium (may need to be adjusted later)
+- dont assign public IP (will use elastic IP)
+- storage = 256GB
+- create new security group "hprc-assembly-hub"
+
+Be sure to associate an elastic IP address to the instance as we use a DNS "A" record for hprc-browser.ucsc.edu pointing to the elastic IP.
+
+## Prepare For Run
+1. Install the AWS CLI:
+```
+sudo apt install awscli -y
+```
+There is no need to setup permissions, we are pulling files w/out egress fees
+
+2. Get the files from this repo
+```
+cd ~
+git clone https://github.com/juklucas/HPRC_Assembly_Hub.git
+```
+And set the alias HUB_REPO
+```
+echo 'export HUB_REPO=/home/ubuntu/github/HPRC_Assembly_Hub' >> ~/.bashrc 
+source .bashrc
+```
+
+## Create The Hub
+
+### Copy Over Marina's Hub
+bash $HUB_REPO/backbone/setup_instance.sh
+
+## Add Repeat Masker Tracks
+bash $HUB_REPO/repeat_masker/create_repeat_masker_track.sh
--- repeat_masker/create_repeat_masker_track.sh
+++ repeat_masker/create_repeat_masker_track.sh
@@ -7,6 +7,10 @@
 
 ## Get data from S3 submission
 cd ~
+
+mkdir repeat_masker_tmp
+cd repeat_masker_tmp
+
 aws --no-sign-request s3 cp \
     --recursive \
     s3://human-pangenomics/submissions/6C63D998-712A-480D-8BEC-99DD8DBE16C5--RM_BEDS/ \
@@ -29,9 +33,9 @@ do
     fi
 
     ## Strip off sample name and haplotype int (to match chrom.sizes file)
-    sed 's/^.*#//' \
+    sed 's/^.*#\(J.*\)/\1/' \
         ${SAMPLE}/assemblies/year1_f1_assembly_v2_genbank/repeat_masker/${SAMPLE}.${HAP_STR}.f1_assembly_v2_genbank_rm.bed \
-        > ${SAMPLE}.${HAPLOTYPE}.rm.bed
+        > ${SAMPLE}/${SAMPLE}.${HAPLOTYPE}.rm.bed
 
     ## I think the type should actually be bed6+4, but I get an error saying: 
     ## Error line 35 of HG00438.1.rm.bed: score (1209) must be between 0 and 1000
@@ -39,15 +43,25 @@ do
         -extraIndex=name \
         -type=bed4+6 \
         -tab \
-        ${SAMPLE}.${HAPLOTYPE}.rm.bed \
+        ${SAMPLE}/${SAMPLE}.${HAPLOTYPE}.rm.bed \
         -as=${HUB_REPO}/repeat_masker/repeat_masker.as \
         /var/www/html/hub/$ASSEMBLY/chrom.sizes \
         /var/www/html/hub/$ASSEMBLY/repeat_masker.bb
 
 
     ## copy over repeat masker trackDb and add to main trackDb.txt file
-    cp ${HUB_REPO}/repeat_masker/repeat_masker_trackDb.txt /var/www/html/hub/$ASSEMBLY/repeat_masker_trackDb.txt
-    sed -i '1 i\include repeat_masker_trackDb.txt' /var/www/html/hub/$ASSEMBLY/trackDb.txt
+    cp ${HUB_REPO}/repeat_masker/repeat_masker_trackDb.txt /var/www/html/hub/$ASSEMBLY/repeat_masker_trackDb.txt 
+
+    ## Add import statement if it's not already there
+    if grep -q 'include repeat_masker_trackDb.txt' /var/www/html/hub/$ASSEMBLY/trackDb.txt; then
+        echo found
+    else
+        sed -i '1 i\include repeat_masker_trackDb.txt' /var/www/html/hub/$ASSEMBLY/trackDb.txt
+    fi
 
 done
+
+
+cd ~
+sudo rm repeat_masker_tmp
 
