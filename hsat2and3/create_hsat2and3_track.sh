559fc8c573257be18aa6d75e2064a4d7ad60f8f7
Julian Lucas <juklucas@ucsc.edu>
2022-04-21 22:34:19 -0700

    add HSat2/HSat3

---
+++ hsat2and3/create_hsat2and3_track.sh
@@ -0,0 +1,65 @@
+## requires AWS CLI
+## must have alias HUB_REPO set
+
+############################################################################### 
+##                             Create BigBeds                                ##
+###############################################################################
+
+## Get data from S3 submission
+cd ~
+
+mkdir hsat2and3_tmp
+cd hsat2and3_tmp
+
+aws --no-sign-request s3 cp \
+    --recursive \
+    s3://human-pangenomics/submissions/1A6CA334-DAF0-4186-AB3E-12442503F2BE--HSAT_2_3/ \
+    .
+
+
+readarray -t ASSEMBLIES <${HUB_REPO}/assembly_info/assembly_list.txt
+
+
+for ASSEMBLY in "${ASSEMBLIES[@]}"
+do 
+    ## extract sample name and haplotype
+    SAMPLE=`echo "$ASSEMBLY" | cut -d'.' -f1`
+    HAPLOTYPE=`echo "$ASSEMBLY" | cut -d'.' -f2`
+
+    if [[ $HAPLOTYPE == 1 ]]; then
+        HAP_STR=paternal
+    else 
+        HAP_STR=maternal
+    fi
+
+    ## Strip off sample name and haplotype int (to match chrom.sizes file)
+    sed 's/^.*#\(J.*\)/\1/' \
+        ${SAMPLE}.${HAP_STR}.f1_assembly_v2_genbank.HSat2and3_Regions.bed \
+        > ${SAMPLE}.${HAPLOTYPE}.hsat2and3.stripped.bed
+
+    bedToBigBed \
+        -extraIndex=name \
+        -type=bed9 \
+        -tab \
+        ${SAMPLE}.${HAPLOTYPE}.hsat2and3.stripped.bed \
+        -as=${HUB_REPO}/hsat2and3/hsat2and3.as \
+        /var/www/html/hub/$ASSEMBLY/chrom.sizes \
+        /var/www/html/hub/$ASSEMBLY/hsat.bb
+
+
+    ## copy over hsat2and3 trackDb and add to main trackDb.txt file
+    cp ${HUB_REPO}/hsat2and3/hsat2and3_trackDb.txt /var/www/html/hub/$ASSEMBLY/hsat2and3_trackDb.txt 
+
+    ## Add import statement if it's not already there
+    if grep -q 'include hsat2and3_trackDb.txt' /var/www/html/hub/$ASSEMBLY/trackDb.txt; then
+        echo found
+    else
+        sed -i '1 i\include hsat2and3_trackDb.txt' /var/www/html/hub/$ASSEMBLY/trackDb.txt
+    fi
+
+done
+
+
+cd ~
+rm flagger_tmp
+
---
+++ hsat2and3/hsat2and3.as
@@ -0,0 +1,13 @@
+table HSat2_HSat3
+"HSat2 and HSat3 arrays"
+    (
+    string chrom;    "Genomic sequence name"
+    uint chromStart;     "Start in genomic sequence"
+    uint chromEnd;       "End in genomic sequence"
+    string type;     "HSat2 or HSat3"
+    uint score;       "always 0 place holder"
+    char[1] strand;     "Relative orientation + or -"
+    uint thickStart;   "Start of where display should be thick"
+    uint thickEnd;     "End of where display should be thick"
+    uint reserved;     "color"
+    )
